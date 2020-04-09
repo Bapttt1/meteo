e65fba71274391a08fe935a9be6def118efe5e30
Corentin <corentin25200@gmail.com>
2020-04-09 19:07:17 +0200

    ajax request

--- Projet météo/Script.js
+++ Projet météo/Script.js
@@ -1,23 +1,24 @@
-function aux (nom){
-    $ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?q=" + nom.trim() + "&APPID=ee07e2bf337034f905cde0bdedae3db8&units=metric&lang=fr",
-        type: "GET",
-        data:  getdata,
-        success: function(data, textStatus, jqXhr) {
-                 var result = data.GetTimeStringResult;
-        }
-    });
-    alert(data);
+function sendRequest (nom){
+    console.log(nom)
+    $.ajax({
+        type:"GET",
+        url:"http://api.openweathermap.org/data/2.5/weather?q=" + nom + ",fr&APPID=ee07e2bf337034f905cde0bdedae3db8",
+        success: function(data) {
+            console.log(data)
+            displayData(data);
+        },
+      });
+}
+
+function displayData (data){
+    $(".temps").append(data.weather[0].main);
 }
 
 $(document).ready(function(){
-    $("#submit").click(function(){
+
+    $("#submit").click(function(e){
+        e.preventDefault();
         let value = $("#ville").val();
-        aux(value);
+        sendRequest(value);
     });
 });
-
-
-function retour(json){
-    console.log(json);
-}
