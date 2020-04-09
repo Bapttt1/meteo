614aba869b5d42ceab5938da1ee19dc34b58d523
Corentin <corentin25200@gmail.com>
2020-04-09 19:26:00 +0200

    error

--- Projet météo/Script.js
+++ Projet météo/Script.js
@@ -1,13 +1,16 @@
 function sendRequest (nom){
-    console.log(nom)
     $.ajax({
         type:"GET",
         url:"http://api.openweathermap.org/data/2.5/weather?q=" + nom + ",fr&APPID=ee07e2bf337034f905cde0bdedae3db8",
         success: function(data) {
-            console.log(data)
+            //console.log(data)
             displayData(data);
         },
-      });
+
+        error : function(resultat, statut, erreur){
+            console.log("erreur " + resultat.status + " " + erreur);
+        }
+    });
 }
 
 function displayData (data){
